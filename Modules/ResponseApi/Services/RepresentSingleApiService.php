c147dbf3480bd6178991c8dd0cc4065be729d63a
Javad Geravand <66083659+gener0us@users.noreply.github.com>
2022-09-12 14:53:54 +0430

    Create RepresentSingleApiService.php
    this Service for save or update data entry in Database. this is Dependency Injection Pattern.

---
+++ Modules/ResponseApi/Services/RepresentSingleApiService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\ResponseApi\Services;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Modules\ResponseApi\DTO\ResponseApiDto;
+
+class RepresentSingleApiService
+{
+    public function handle(ResponseApiDto $dto): User
+    {
+        // create or update in DB and return User
+        $user = User::updateOrCreate(
+            [
+                'email' => $dto->getEmail(),]
+            , [
+            'name' => $dto->getName(),
+            'password' => Hash::make($dto->getKey())
+        ]);
+        return $user;
+    }
+}
